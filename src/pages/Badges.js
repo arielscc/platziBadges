561fe6c862b81bcf8cfacbe363f61f7f6592ee00
Ariel Chura <lf0ariel@gmail.com>
2020-08-26 22:15:44 -0400

    Solicitando datos (GET)

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -1,45 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import confLogo from '../images/badge-header.svg';
 
 import './Badges.css';
 import BadgesList from '../components/BadgesList/BadgesList';
 import { Link } from 'react-router-dom';
-
+import api from '../api';
 export default function Badges() {
-  const [state, setstate] = useState({
-    data: [
-      {
-        id: '2de30c42-9deb-40fc-a41f-05e62b5939a7',
-        irstName: 'Freda',
-        lastName: 'Grady',
-        email: 'Leann_Berge@gmail.com',
-        jobTitle: 'Legacy Brand Director',
-        twitter: 'FredaGrady22221-7573',
-        avatarUrl: 'https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon',
-      },
-      {
-        id: 'd00d3614-101a-44ca-b6c2-0be075aeed3d',
-        firstName: 'Major',
-        lastName: 'Rodriguez',
-        email: 'Ilene66@hotmail.com',
-        jobTitle: 'Human Research Architect',
-        twitter: 'ajorRodriguez61545',
-        avatarUrl: 'https://www.gravatar.com/avatar/d57a8be8cb9219609905da25d5f3e50a?d=identicon',
-      },
-      {
-        id: '63c03386-33a2-4512-9ac1-354ad7bec5e9',
-        firstName: 'Daphney',
-        lastName: 'Torphy',
-        email: 'Ron61@hotmail.com',
-        jobTitle: 'National Markets Officer',
-        twitter: 'DaphneyTorphy96105',
-        avatarUrl: 'https://www.gravatar.com/avatar/e74e87d40e55b9ff9791c78892e55cb7?d=identicon',
-      },
-    ],
+  const [state, setState] = useState({
+    loading: true,
+    error: null,
+    data: [],
   });
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  const fetchData = async () => {
+    try {
+      const data = await api.badges.list();
+      setState({ loading: false, data });
+    } catch (error) {
+      setState({ loading: false, error });
+    }
+  };
+
   return (
-    <div>
+    <>
       <div className="Badges">
         <div className="Badges__hero">
           <div className="Badges__container">
@@ -53,12 +41,14 @@ export default function Badges() {
             New Badge
           </Link>
         </div>
+        {state.loading && <p>Loading....</p>}
         <div className="Badges__list">
           <div className="Badges__container">
             <BadgesList badges={state.data} />
           </div>
         </div>
+        {state.error && <p>Error {state.error}</p>}
       </div>
-    </div>
+    </>
   );
 }
